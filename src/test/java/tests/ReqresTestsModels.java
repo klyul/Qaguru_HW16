544bfe2db44e6279d6d79d5f9390e903999e0a1f
klyul <yuliya.v3333@gmail.com>
2023-05-01 10:58:11 +0300

    HW16

---
+++ src/test/java/tests/ReqresTestsModels.java
@@ -0,0 +1,113 @@
+package tests;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DisplayName("Reqres tests models")
+@Tag("API")
+
+public class ReqresTestsModels {
+
+        /*
+        1. Make request (POST) to https://reqres.in/api/login with body { "email": "eve.holt@reqres.in", "password": "cityslicka" }
+        2. Get response { "token": "QpwL5tke4Pnpja7X4" }
+        3. Check token is QpwL5tke4Pnpja7X4
+     */
+
+    @Test
+    @DisplayName("Create user")
+    void createUserTest() {
+        String body = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+        given()
+                .log().uri()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+
+    }
+
+
+    @Test
+    @DisplayName("Successful login")
+    void successfulLoginTest() {
+    LoginBodyLombokModel loginBody = new LoginBodyLombokModel();
+        loginBody.setEmail("eve.holt@reqres.in");
+        loginBody.setPassword("cityslicka");
+
+    LoginResponseLombokModel response = given()
+            .log().uri()
+            .body(loginBody)
+            .contentType(JSON)
+            .when()
+            .post("https://reqres.in/api/login")
+            .then()
+            .log().status()
+            .log().body()
+            .statusCode(200)
+            .extract().as(LoginResponseLombokModel.class);
+
+    assertThat(response.getToken()).isEqualTo("QpwL5tke4Pnpja7X4");
+}
+
+    @Test
+    @DisplayName("Unsuccessful login")
+    void unSuccessfulLoginWithMissingEmailTest() {
+        String body = "{ \"password\": \"cityslicka\" }";
+
+        given()
+                .log().uri()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+    }
+
+    @Test
+    @DisplayName("Unsuccessful login with missing password")
+    void unSuccessfulLoginWithMissingPasswordTest() {
+        String body = "{ \"email\": \"eve.holt@reqres.in\" }";
+
+        given()
+                .log().uri()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+
+    @Test
+    @DisplayName("Unsuccessful login with empty data")
+    void unSuccessfulLoginWithEmptyDataTest() {
+        given()
+                .log().uri()
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(415);
+    }
+
+}
